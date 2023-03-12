b2e95699d8fb66b99647a9e5e597a9bad92148db
Gsingh704 <gurjantaulakh704@gmail.com>
2023-03-12 09:56:00 +0000

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#c707ab"
+    }
+
   },
   plugins: [],
 }
